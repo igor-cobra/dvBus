625408dc4472052fb7624b65771bbeef12669063
Igor Cobra <igor.cobra.kp@gmail.com>
2022-06-22 14:12:45 -0300

    Dependencias
    Adicionado dependência para o projeto

--- App.tsx
+++ App.tsx
@@ -6,3 +6,20 @@ import {
   Nunito_700Bold,
   Nunito_800ExtraBold
 } from '@expo-google-fonts/nunito';
+
+import AppStack from './src/routes/AppStack';
+
+export default function App() {
+  let [fontsLoadded] = useFonts({ Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold, });
+
+  if (!fontsLoadded) {
+    return null;
+  } else {
+    return (
+      <>
+        <StatusBar backgroundColor="transparent" translucent barStyle="dark-content"/>
+        <AppStack/>
+      </>
+    )
+  }
+}
