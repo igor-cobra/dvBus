5dbd4465b4766c7c1ee0b93308c5bbb9c9d143c8
Igor Cobra <igor.cobra.kp@gmail.com>
2022-06-23 11:51:29 -0300

    Finalização Header
    Finalização do Header para ser herdado pelas telas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { StackHeaderProps } from '@react-navigation/stack';
 
-import './styles.css';
+import HeaderStyles from '../styles/components/HeaderStyles';
 
 interface HeaderProps extends StackHeaderProps {
-  showCancel?: boolean;
+  showBack?: boolean;
   title: string;
 }
 
-export default function Header({ showCancel = true, title, navigation }: HeaderProps) {
-  function handleCancelCreateBusStop() {
-    navigation.navigate('BusPointsMap');
-  }
-
+export default function Header({ showBack = true, title, navigation }: HeaderProps) {
   return (
-    <View style={styles.container}>
+    <View style={HeaderStyles.container}>
+      <BorderlessButton onPress={navigation.goBack}>
+        <Feather name="arrow-left" size={24} color="#15B6D6"/>
+      </BorderlessButton>
+
+      <Text style={HeaderStyles.title}>{title}</Text>
+
+      {showBack?(
+        <BorderlessButton onPress={navigation.goBack}>
+          <Feather name="x" size={24} color="#FF669D"/>
+        </BorderlessButton>      
+      ) : (
+        <View/>
+      )}
     </View>
   )
 }
